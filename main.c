6d5827f74a80ebab65aacdea54f8053920847a0b
Lauri Pihlajakangas <lauri.pihlajakangas@gmail.com>
2019-04-16 16:29:43 +0300

    messed up better code

--- main.c
+++ main.c
@@ -7,12 +7,23 @@
 
 #define WIDTH 320
 #define HEIGHT 240
+#define PIXELSIZE 4 // 4 bytes rgba
 
-uint8_t pixel[4];
-uint8_t buffer[WIDTH*HEIGHT*sizeof(pixel)];
+//uint8_t buffer[WIDTH*HEIGHT*PIXELSIZE];
+uint8_t buffer[WIDTH][HEIGHT][PIXELSIZE];
+
+enum rgba {
+    R=0,
+    G=1,
+    B=2,
+    A=3,
+};
 
 int main()
 {
+    int16_t x,y;
+    int8_t c;
+
     char file[] = "image2.bmp";
 
     int8_t ret;
@@ -21,13 +32,17 @@ int main()
     uint32_t size = sizeof(buffer);
     uint32_t iterator = 0;
 
-    while(iterator < size)
+    for(y=0; y<HEIGHT; y++)
     {
-        // try blue color
-        buffer[iterator] = iterator ^ (iterator/HEIGHT);
-
-        iterator++;
-    } 
+        for(x=0; x<WIDTH; x++)
+        {
+            c = x^y;
+            buffer[x][y][R] = c;
+            buffer[x][y][G] = c;
+            buffer[x][y][B] = c;
+            buffer[x][y][A] = 0xFF;
+        }
+    }
 
     ret = stbi_write_bmp(file, WIDTH, HEIGHT, 4, &buffer);
 
