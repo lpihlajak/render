c658d433f68853cb0918d0c34d554de71617cf9a
Lauri Pihlajakangas <lauri.pihlajakangas@gmail.com>
2019-04-16 09:42:41 +0300

    rgb works

--- main.c
+++ main.c
@@ -13,28 +13,23 @@ uint8_t buffer[WIDTH*HEIGHT*sizeof(pixel)];
 
 int main()
 {
-    char file[] = "image.bmp";
+    char file[] = "image2.bmp";
 
     int8_t ret;
     printf("hello");
 
     uint32_t size = sizeof(buffer);
-    uint32_t iterator = size;
+    uint32_t iterator = 0;
 
-    while(iterator > 0)
+    while(iterator < size)
     {
-        if(iterator % sizeof(pixel) == 0) 
-        {
-            buffer[iterator] = 0xFF000055;
-        }
-        else
-        {
-            buffer[iterator] = iterator ^ (iterator/HEIGHT);
-        }
-        iterator--;
+        // try blue color
+        buffer[iterator] = iterator ^ (iterator/HEIGHT);
+
+        iterator++;
     } 
 
-    ret = stbi_write_bmp(file, WIDTH, HEIGHT, 1, &buffer);
+    ret = stbi_write_bmp(file, WIDTH, HEIGHT, 4, &buffer);
 
     if(ret !=0) {
         printf("error");
