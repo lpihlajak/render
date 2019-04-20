551b15fab83fee045912f2dc82ecb40e0d310f9b
Lauri Pihlajakangas <lauri.pihlajakangas@gmail.com>
2019-04-20 23:33:42 +0300

    animation!

---
+++ anim/anim.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "stb_image_write.h"
+
+#define WIDTH 640
+#define HEIGHT 480
+
+#define PIXELSIZE 4
+
+#define TOTAL_FRAMES 240
+
+uint8_t buffer[WIDTH*HEIGHT*PIXELSIZE];
+//uint8_t buffer[WIDTH][HEIGHT][PIXELSIZE];
+
+enum rgba {
+    R=0,
+    G=1,
+    B=2,
+    A=3,
+};
+
+void setPixel(uint32_t x, uint32_t y, uint8_t r, uint8_t g, uint8_t b, uint8_t a, uint8_t *buf)
+{
+    uint32_t addr = (x*PIXELSIZE) + y*PIXELSIZE*WIDTH;
+    if(addr < WIDTH*HEIGHT*PIXELSIZE)
+    {
+        buf[addr+R] = r; 
+        buf[addr+G] = g; 
+        buf[addr+B] = b; 
+        buf[addr+A] = a; 
+    }
+    else
+    {
+        printf("memory error");
+    }   
+    return;
+}
+void clear(uint8_t *buffer)
+{
+    uint32_t x;
+    uint32_t y;
+    for(y=0;y<HEIGHT;y++)
+    {
+        for(x=0;x<WIDTH;x++)
+        {
+            setPixel(x,y,0x00,0x00,0x00,0xFF, buffer);
+        }
+    }
+}
+
+float meta(uint32_t x, uint32_t y)
+{
+    return (1.0 / (x*x + y*y)); 
+}
+int main()
+{
+    int8_t c;
+
+    float amplitude;
+
+    char file[32];
+
+    uint32_t x;
+    uint32_t y;
+
+    uint32_t meta1x = 150;
+    uint32_t meta1y = 150;
+
+    uint32_t meta2x = 450;
+    uint32_t meta2y = 250;
+
+    uint32_t i = 0;
+
+    int8_t ret;
+    printf("hello");
+
+    while(i< TOTAL_FRAMES)
+    {
+        clear(buffer);
+
+        for(y=0;y<HEIGHT;y++)
+        {
+            for(x=0;x<WIDTH;x++)
+            {
+                //setPixel(x,y,0xFF,0xFF,0x00,0xFF, buffer);
+                amplitude = meta(x-meta1x,y-meta1y); 
+                amplitude = amplitude + meta(x-meta2x,y-meta2y); 
+                //printf("\namplitude %f", amplitude);
+                if(amplitude > 0.000052)
+                {
+                    setPixel(x,y,0x00,(uint8_t)((float)0xFF*amplitude*20000),0x00,0xFF, buffer);
+                }
+            }
+        }
+
+        snprintf(file, sizeof(char) * 32, "frames/image%i.png", i);
+        printf("\nRendering frame %s", file);
+        ret = stbi_write_png(file, WIDTH, HEIGHT, 4, &buffer, WIDTH*PIXELSIZE);
+        meta1x++;
+        meta2y--;
+        i++;
+    }
+
+    if(ret !=0) {
+        printf("error");
+        return 1;
+    }
+    else printf("ok");
+
+    return 0;
+}
