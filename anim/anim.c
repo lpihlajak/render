165c22ef22a8805c530a15dc003de6f3ac12020c
Lauri Pihlajakangas <lauri.pihlajakangas@gmail.com>
2019-05-13 10:34:56 +0300

    Metaball thing with linear motion and ffmpeg script

--- anim/anim.c
+++ anim/anim.c
@@ -10,7 +10,7 @@
 
 #define PIXELSIZE 4
 
-#define TOTAL_FRAMES 240
+#define TOTAL_FRAMES 2400
 
 uint8_t buffer[WIDTH*HEIGHT*PIXELSIZE];
 //uint8_t buffer[WIDTH][HEIGHT][PIXELSIZE];
@@ -51,10 +51,23 @@ void clear(uint8_t *buffer)
     }
 }
 
-float meta(uint32_t x, uint32_t y)
+float metaCalc(uint32_t x, uint32_t y)
 {
     return (1.0 / (x*x + y*y)); 
 }
+// objects
+
+struct Vec2 {
+  float x;
+  float y;
+} Vec2;
+
+typedef struct Meta {
+  uint32_t x;
+  uint32_t y;
+  struct Vec2 v;  // Velocity
+} Rect;
+
 int main()
 {
     int8_t c;
@@ -66,11 +79,17 @@ int main()
     uint32_t x;
     uint32_t y;
 
-    uint32_t meta1x = 150;
-    uint32_t meta1y = 150;
+    struct Meta meta[2];
 
-    uint32_t meta2x = 450;
-    uint32_t meta2y = 250;
+    meta[0].x = 150;
+    meta[0].y = 150;
+    meta[0].v.x = 1; 
+    meta[0].v.y = 1; 
+
+    meta[1].x = 450;
+    meta[1].y = 250;
+    meta[0].v.x = 1; 
+    meta[0].v.y = 1; 
 
     uint32_t i = 0;
 
@@ -86,8 +105,8 @@ int main()
             for(x=0;x<WIDTH;x++)
             {
                 //setPixel(x,y,0xFF,0xFF,0x00,0xFF, buffer);
-                amplitude = meta(x-meta1x,y-meta1y); 
-                amplitude = amplitude + meta(x-meta2x,y-meta2y); 
+                amplitude = metaCalc(x-meta[0].x,y-meta[0].y); 
+                amplitude = amplitude + metaCalc(x-meta[1].x,y-meta[1].y); 
                 //printf("\namplitude %f", amplitude);
                 if(amplitude > 0.000052)
                 {
@@ -99,8 +118,18 @@ int main()
         snprintf(file, sizeof(char) * 32, "frames/image%i.png", i);
         printf("\nRendering frame %s", file);
         ret = stbi_write_png(file, WIDTH, HEIGHT, 4, &buffer, WIDTH*PIXELSIZE);
-        meta1x++;
-        meta2y--;
+
+        //int32_t xdir = meta[0].x - meta[1].x;
+        //int32_t ydir = meta[0].y - meta[1].y;
+        int32_t xdir = 1;
+        int32_t ydir = 1;
+
+        meta[0].x += xdir*meta[0].v.x; 
+        meta[0].y += xdir*meta[0].v.y; 
+
+        meta[1].x += xdir*meta[1].v.x; 
+        meta[1].y += xdir*meta[1].v.y; 
+
         i++;
     }
 
