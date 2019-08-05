b1f6fed2eeffd88752b380585488d5c8667d8e89
Lauri Pihlajakangas <lauri.pihlajakangas@gmail.com>
2019-08-05 21:15:33 +0300

    Implemented leapfrog calc, gravity limit to 0.3

--- anim/anim.c
+++ anim/anim.c
@@ -52,15 +52,15 @@ void clear(uint8_t *buffer)
     }
 }
 
-float metaCalc(uint32_t x, uint32_t y)
+double metaCalc(uint32_t x, uint32_t y)
 {
     return (1.0 / (x*x + y*y)); 
 }
 // objects
 
 struct Vec2 {
-    float x;
-    float y;
+    double x;
+    double y;
 } Vec2;
 
 typedef struct Meta {
@@ -72,9 +72,9 @@ int main()
 {
     int8_t c;
 
-    float amplitude1;
-    float amplitude2;
-    float amplitudeSum;
+    double amplitude1;
+    double amplitude2;
+    double amplitudeSum;
 
     char file[32];
 
@@ -115,11 +115,11 @@ int main()
 
                 amplitudeSum = amplitude1 + amplitude2;             
 
-                color = (uint8_t)((float)0xFF*amplitudeSum*20000);
+                color = (uint8_t)((double)0xFF*amplitudeSum*20000);
     
                 if(amplitudeSum > 0.000052)
                 {
-                    setPixel(x,y,0x00,(uint8_t)((float)color*amplitude1*COLOR_INTENSITY),(uint8_t)((float)color*amplitude2*COLOR_INTENSITY),0xFF, buffer);
+                    setPixel(x,y,0x00,(uint8_t)((double)color*amplitude1*COLOR_INTENSITY),(uint8_t)((double)color*amplitude2*COLOR_INTENSITY),0xFF, buffer);
                 }
             }
         }
@@ -135,27 +135,19 @@ int main()
         int32_t yDelta = (meta[1].pos.y - meta[0].pos.y);
 
         // TODO: Hack to fix behavior where both objects would shoot out to space when near each other..
-        if((abs(xDelta) < 5) && (abs(yDelta) < 5))
-        {
-            meta[0].v.x -= 0.02; 
-            meta[0].v.y -= 0.02; 
-            meta[1].v.x -= 0.02; 
-            meta[1].v.y -= 0.02; 
-            iter++; continue;
-        }
 
-        int32_t i;
-        int32_t j;
+        int32_t i; // direction vector for x
+        int32_t j; // direction vector for y
 
-        float gravity;
+        double gravity;
 
         xDelta = (meta[1].pos.x - meta[0].pos.x);
         yDelta = (meta[1].pos.y - meta[0].pos.y);
     
 
         gravity = metaCalc(xDelta, yDelta)*1000; // *1000 makes gravity 1 when overlapping
-        if(gravity > 1)
-            gravity = 1.0;
+        if(gravity > 0.3)
+            gravity = 0.3;
 
         if(xDelta > 0)
             i = 1;
@@ -167,11 +159,11 @@ int main()
         else
             j = -1;
 
+        meta[0].pos.x += meta[0].v.x + i*gravity/2.0f; // calculate position
         meta[0].v.x += i*gravity; // calc speed
-        meta[0].pos.x += meta[0].v.x; // calculate position
 
+        meta[0].pos.y += meta[0].v.y + j*gravity/2.0f; // calculate position
         meta[0].v.y += j*gravity; // calc speed
-        meta[0].pos.y += meta[0].v.y; // calculate position
 
         printf("\nX:%fY:%f v.x: %f, v.y: %f, i: %d, j: %d, xDelta: %d yDelta: %d Gravity: %f", meta[0].pos.x, meta[0].pos.y, meta[0].v.x, meta[0].v.y, i, j, xDelta, yDelta, gravity);
 
@@ -180,8 +172,8 @@ int main()
 
 
         gravity = metaCalc(xDelta, yDelta)*1000; // *1000 makes gravity 1 when overlapping
-        if(gravity > 1)
-            gravity = 1.0;
+        if(gravity > 0.3)
+            gravity = 0.3;
 
         if(xDelta > 0)
             i = 1;
@@ -193,11 +185,14 @@ int main()
         else
             j = -1;
 
-        meta[1].v.x += i*gravity; // calc speed
-        meta[1].pos.x += meta[1].v.x; // calculate position
+//position = position + velocity * timestep + acceleration/2.0f * timestep
+//velocity = velocity + acceleration * timestep
 
+        meta[1].pos.x += meta[1].v.x + i*gravity/2.0f; // calculate position
+        meta[1].v.x += i*gravity; // calc speed
+
+        meta[1].pos.y += meta[1].v.y + j*gravity/2.0f; // calculate position
         meta[1].v.y += j*gravity; // calc speed
-        meta[1].pos.y += meta[1].v.y; // calculate position
 
         printf("\nX:%fY:%f v.x: %f, v.y: %f, i: %d, j: %d, Delta: %d yDelta: %d Gravity: %f", meta[1].pos.x, meta[1].pos.y, meta[1].v.x, meta[1].v.y, i, j, xDelta, yDelta, gravity);
 
